1217131a10db2ebe280bc7ace68c19e523c8f97f
dayron23 <dayronf30@gmail.com>
2023-02-22 12:03:57 -0500

    commit primera entrega

---
+++ main.js
@@ -0,0 +1,61 @@
+class ProductManager {
+    constructor(){
+        this.products = [];
+    }
+
+    static id = 0
+
+    addProduct(title, description, price, image, code, stock){
+        for(let i = 0; i < this.products.length; i++){
+            if(this.products[i].code == code){
+                console.log(`El codigo ${code} esta repetido`);
+                break;
+            }
+        }
+
+        const newProduct = {
+            title,
+            description, 
+            price, 
+            image, 
+            code, 
+            stock,
+        }
+
+        if(!Object.values(newProduct).includes(undefined)){
+            ProductManager.id++;
+            this.products.push({ 
+                ...newProduct,
+                id:ProductManager.id,
+            });
+        } else{
+            console.log("Todos los campos son requeridos")
+        }
+    }
+
+    getProduct(){
+    return this.products;
+    }
+
+    existe (id){
+        return this.products.find((producto) => producto.id === id)
+    }
+
+    getProductById(id){
+        !this.existe(id) ? console.log("not found") : console.log(this.existe(id));
+    }
+}
+
+const productos = new ProductManager();
+
+console.log(productos.getProduct());
+
+productos.addProduct("titulo1", "descripcion1", 1000, "imagen1", "abc123", 5 );
+productos.addProduct("titulo2", "descripcion2", 1000, "imagen2", "abc125", 7 );
+
+
+console.log(productos.getProduct());
+
+productos.addProduct("titulo3", "descripcion3", 1000, "imagen3", "abc124", 7 );
+
+productos.getProductById(2)
